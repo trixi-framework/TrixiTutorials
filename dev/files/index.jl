578808c324474c34e8b97ced026f92ab453613b3
Documenter.jl <documenter@juliadocs.github.io>
2021-07-20 10:54:22 +0000

    build based on 27cc398

---
+++ dev/files/index.jl
@@ -0,0 +1,14 @@
+# # Tutorials für Trixi.jl
+
+# Here, TrixiTutorials - a new feature of [trixi-framework](https://github.com/trixi-framework) - will occur soon.
+# It will contain a tutorial section for Trixi.jl with interactive step-by-step explanations via [Binder](https://mybinder.org).
+# Since we will reset the repository history before the official start, please be aware of possible changes in the code and URLs.
+
+# We are working on it.
+
+#src Working with somebody else's code often is hard in the beginning. To simplify the start looking into an example might help. It is even more helpful if you can run the example by yourself and see what single lines or some commands will do.
+#src So, in this tutorial section you have the possibility to do exactly this.
+#src There are three ways to use these tutorials:
+#src - If you have not much time or just want to look quickly in the example, you can use the documentation page. 
+#src - For a deeper look there is a not interactive way with `nbviewer` where the code is already executed.
+#src - To take full advantage you can use the interactive notebook version of the tutorials where you can run the code by yourself. To get there just click on the `binder` logo within the corresponding tutorial.
