3f42365c25d3b4f9046d1dd9c33f0fe3b8fe7b07
Benjamin Bolm <benjamin.bolm@gmx.de>
2021-07-20 13:20:23 +0200

    Fix links, add 2files1topic-feature

--- docs/make.jl
+++ docs/make.jl
@@ -4,10 +4,34 @@ using Literate
 import Pkg
 using Trixi
 
-# Switch variable if documentation should be build if URLs are not valid.
-switch = true
+# Function to create markdown and notebook files for specific file
+function create_files(title, file; folder="")
+    notebook_filename = "$(splitext(file)[1]).ipynb"
 
-# Creating tutorials for these files:
+    binder_badge   = string("# [![]($binder_logo)](",   joinpath(binder_url,   folder, notebook_filename), ")")
+    nbviewer_badge = string("# [![]($nbviewer_logo)](", joinpath(nbviewer_url, folder, notebook_filename), ")")
+    download_badge = string("# [![]($download_logo)](", joinpath(download_url, folder, notebook_filename), ")")
+    
+    # Generate notebooks
+    function preprocess_notebook(content)
+        return string("# # $title\n\n", preprocess_links(content))
+    end
+    Literate.notebook(joinpath(repo_src, folder, file), joinpath(notebooks_dir, folder); preprocess=preprocess_notebook,)
+
+    # Generate markdowns
+    function preprocess_docs(content)
+        return string("# # $title\n $binder_badge\n $nbviewer_badge\n $download_badge\n\n", preprocess_links(content))
+    end
+    Literate.markdown(joinpath(repo_src, folder, file), joinpath(pages_dir, folder); documenter=false,
+                      execute=true, preprocess=preprocess_docs,)
+
+end
+
+# Creating tutorials for the following files:
+# Normal structure: "title" => "filename.jl"
+# If there are several files for one topic and folder, the structure is:
+#   "title" => ["subtitle 1" => ("folder 1", "filename 1.jl"),
+#               "subtitle 2" => ("folder 2", "filename 2.jl")]
 files = [
     "Adding a new equation" => "adding_a_new_equation.jl",
     "Differentiable programming" => "differentiable_programming.jl",
@@ -24,91 +48,58 @@ Sys.rm(notebooks_dir;   recursive=true, force=true)
 # Preprocess files to add reference web links automatically.
 trixi_version = Pkg.dependencies()[Pkg.project().dependencies["Trixi"]].version
 trixi_link = "https://trixi-framework.github.io/Trixi.jl/v$trixi_version/"
+
+# Function that replaces `@trixi-docs` and `@trixi-ref` with the links to the Trixi documentation
 function preprocess_links(content)
     # Replacing `@trixi-docs:` in `content` with the defined `trixi_link`
     content = replace(content, "@trixi-docs:" => trixi_link)
     # Searching for `[`Example`](@trixi-ref)` in content and replace it with `[`Example`](trixi_link/reference_trixi/#Trixi.Example)`.
-    content = replace(content, r"\[`(?<ref>\w+)`\]\(@trixi-ref\)" => SubstitutionString("[`(\\g<ref>)`]($(trixi_link)reference-trixi/#Trixi.\\g<ref>)"))
+    content = replace(content, r"\[`(?<ref>\w+)`\]\(@trixi-ref\)" => SubstitutionString("[`\\g<ref>`]($(trixi_link)reference-trixi/#Trixi.\\g<ref>)"))
 end
 
-function postprocess_links(content)
-    if occursin(r"\(https://[^\(\)]+\)", content)
-        matches = collect(eachmatch(r"\(https://[^\(\)]+\)", content))
-        for i in 1:length(matches)
-            link = string(chop(matches[i].match, head=1, tail=1))
-            try 
-                HTTP.get(link, retry=false, connect_timeout=10)
-            catch
-                if switch
-                    @warn "URL doesn't exist: " link
-                else 
-                    error("URL doesn't exist: ", link)
-                end
-            end
-        end
-    end
-    return content
-end
+binder_logo   = "https://mybinder.org/badge_logo.svg"
+nbviewer_logo = "https://raw.githubusercontent.com/jupyter/design/master/logos/Badges/nbviewer_badge.svg"
+download_logo = "https://camo.githubusercontent.com/aea75103f6d9f690a19cb0e17c06f984ab0f472d9e6fe4eadaa0cc438ba88ada/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f646f776e6c6f61642d6e6f7465626f6f6b2d627269676874677265656e"
 
-binder_logo = "https://mybinder.org/badge_logo.svg"
-nbviewer_logo = "https://img.shields.io/badge/show-nbviewer-579ACA.svg"
+# binder_url = joinpath("@__BINDER_ROOT_URL__", "dev/notebooks")
+# nbviewer_url = joinpath("@__NBVIEWER_ROOT_URL__", "dev/notebooks")
 
-binder_url = joinpath("@__BINDER_ROOT_URL__","dev/notebooks")
-@info "" binder_url
-# nbviewer_url = joinpath("@__NBVIEWER_ROOT_URL__","dev/notebooks")
+binder_url   = "https://mybinder.org/v2/gh/trixi-framework/TrixiTutorials/gh-pages?filepath=dev/notebooks/"
+nbviewer_url = "https://nbviewer.jupyter.org/github/trixi-framework/TrixiTutorials/blob/gh-pages/dev/notebooks/"
+download_url = "https://raw.githubusercontent.com/trixi-framework/TrixiTutorials/gh-pages/dev/notebooks/"
 
-binder_url = joinpath("https://mybinder.org/v2/gh/trixi-framework/TrixiTutorials/gh-pages?filepath=dev/notebooks/")
-nbviewer_url = joinpath("https://nbviewer.jupyter.org/github/trixi-framework/TrixiTutorials/blob/gh-pages/dev/notebooks/")
+# Navigation system for makedocs
+pages = []
 
-binder_badge = string("# [![](", binder_logo, ")](", binder_url, ")")
-nbviewer_badge = string("# [![](", nbviewer_logo, ")](", nbviewer_url, ")")
-
-# Add introduction file (index.jl) to navigation menu
-pages = ["Introduction" => "index.md"]
 # Generate markdown for index.jl
-function preprocess_docs(content)
-    return string("# # TrixiTutorials.jl", "\n", binder_badge, "\n", nbviewer_badge, "\n\n",
-                  preprocess_links(content))
-end
-Literate.markdown(joinpath(repo_src, "index.jl"), joinpath(pages_dir, ".."); name="index", documenter=false,
-                  execute=true, preprocess=preprocess_docs, postprocess=postprocess_links)
+Literate.markdown(joinpath(repo_src, "index.jl"), joinpath(pages_dir, ".."); execute=true,
+                  preprocess=preprocess_links,)
 # TODO: With `documenter=false` there is no `link to source` in html file. With `true` the link is not defined because of some `<unkown>`.
+push!(pages, ("Home" => "index.md"))
 
-# Create markdown and notebook files for other src files.
+# Create markdown and notebook files for tutorials.
 for (i, (title, filename)) in enumerate(files)
-    tutorial_title = string("# # Tutorial ", i, ": ", title)
-    tutorial_file = string(splitext(filename)[1])
+    # Several files of one topic are created seperately and pushed to pages together.
+    if filename isa Vector
+        vector = []
+        for j in 1:length(filename)
+            create_files("Tutorial $i.$j: $title: $(filename[j][1])", filename[j][2][2]; folder=filename[j][2][1])
 
-    notebook_filename = string(tutorial_file, ".ipynb")
-    markdown_filename = string(tutorial_file, ".md")
-    
-    binder_badge_ = string("# [![](", binder_logo, ")](", joinpath(binder_url, notebook_filename), ")")
-    nbviewer_badge_ = string("# [![](", nbviewer_logo, ")](", joinpath(nbviewer_url, notebook_filename), ")")
-    
-    # Generate notebooks
-    function preprocess_notebook(content)
-        return string(tutorial_title, "\n\n", preprocess_links(content))
+            push!(vector, "Tut $i.$j: $(filename[j][1])" => joinpath("pages", "$(filename[j][2][1])/$(splitext(filename[j][2][2])[1]).md"))
+        end
+        # Add to navigation menu
+        push!(pages, ("Tut $i: $title" => vector))
+    else # Single files
+        create_files("Tutorial $i: $title", filename)
+        # Add to navigation menu
+        push!(pages, ("Tut $i: $title" => joinpath("pages", "$(splitext(filename)[1]).md")))
     end
-
-    Literate.notebook(joinpath(repo_src,filename), notebooks_dir; name=tutorial_file,
-                      execute=true, preprocess=preprocess_notebook,)
-
-    # Generate markdowns
-    function preprocess_docs(content)
-        return string(tutorial_title, "\n", binder_badge_, "\n", nbviewer_badge_, "\n\n",
-                      preprocess_links(content))
-    end
-    Literate.markdown(joinpath(repo_src, filename), pages_dir; name=tutorial_file, documenter=false,
-                      execute=true, preprocess=preprocess_docs, postprocess=postprocess_links)
-
-    # Add to navigation menu
-    push!(pages, (title => joinpath("pages", markdown_filename)))
 end
 
 # Create documentation with Documenter.jl
 makedocs(
     # Set sitename to Trixi
-    sitename="TrixiTutorials",
+    sitename = "Tutorials for Trixi.jl",
     # Provide additional formatting options
     format = Documenter.HTML(
         # Disable pretty URLs during manual testing
@@ -120,7 +111,8 @@ makedocs(
     ),
     # Explicitly specify documentation structure
     pages = pages,
-    strict = true # to make the GitHub action fail when doctests fail, see https://github.com/neuropsychology/Psycho.jl/issues/34
+    linkcheck = true,
+    # strict = true # to make the GitHub action fail when doctests fail, see https://github.com/neuropsychology/Psycho.jl/issues/34
 )
 
 deploydocs(
